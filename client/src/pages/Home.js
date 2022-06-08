4c97aab7446425fc04d7823cd528e7a028f8dabe
Lindy Briggs <briggs.lindy@yahoo.com>
2022-06-08 10:33:42 -0500

    home page cleared of form and list, moved greeting message from header to home page, changed window.location.assign('/me') to reroute users to their profile page when logged in

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
 
-import ThoughtList from '../components/ThoughtList';
-import ThoughtForm from '../components/ThoughtForm';
-
 import { QUERY_THOUGHTS } from '../utils/queries';
 
 const Home = () => {
@@ -17,17 +14,7 @@ const Home = () => {
           className="col-12 col-md-10 mb-3 p-3"
           style={{ border: '1px dotted #1a1a1a' }}
         >
-          <ThoughtForm />
-        </div>
-        <div className="col-12 col-md-8 mb-3">
-          {loading ? (
-            <div>Loading...</div>
-          ) : (
-            <ThoughtList
-              thoughts={thoughts}
-              title="Some Feed for Thought(s)..."
-            />
-          )}
+          <p className="m-0">Welcome to the Expense Tracker, where monitoring home-project budgets is made easy.</p>
         </div>
       </div>
     </main>
