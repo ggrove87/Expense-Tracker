3e9e3a169b3030bcbdc0ceddad6a25c4dba88969
Lindy Briggs <briggs.lindy@yahoo.com>
2022-06-09 16:48:04 -0500

    add expense form updated with mui components

--- client/src/components/ExpenseForm/index.js
+++ client/src/components/ExpenseForm/index.js
@@ -1,7 +1,16 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { useMutation } from '@apollo/client';
 
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Alert from '@mui/material/Alert';
+
+import { useMutation } from '@apollo/client';
 import { ADD_EXPENSE } from '../../utils/mutations';
 
 import Auth from '../../utils/auth';
@@ -50,6 +59,52 @@ const ExpenseForm = ({ projectId }) => {
       setExpensePrice(intValue); 
     }
   };
+
+  return (
+    <>
+
+{Auth.loggedIn() ? (
+      <form onSubmit={handleFormSubmit}>
+        <Card align="center" sx={{ border: 1, maxWidth: 275, mx: 'auto', mt: '2rem' }}>
+          <CardContent>
+            <Typography color="text.secondary" variant="h5" gutterBottom>What expense are you adding for this project?</Typography>
+
+            <Box sx={{ '& > :not(style)': { m: 1, width: '25ch' }, }} noValidate autoComplete="off">
+
+            <TextField id="outlined-basic" label="name of expense..." variant="outlined"
+                name="expenseText"
+                value={expenseText}
+                onChange={handleChange} />
+
+              <TextField id="outlined-basic" label="# of units purchased..." variant="outlined"
+                name="expenseCount"
+                type="number"
+                value={expenseCount}
+                onChange={handleChange} />
+
+              <TextField id="outlined-basic" label="price per unit..." variant="outlined"
+                name="expensePrice"
+                type="number"
+                value={expensePrice}
+                onChange={handleChange} />
+            </Box>
+
+          </CardContent>
+
+          <CardActions style={{ justifyContent: 'center' }}>
+            <Button size="small" style={{ cursor: 'pointer' }} sx={{ mb: '1rem' }} type="submit" variant="outlined">Add Expense</Button>
+          </CardActions>
+
+        </Card>
+      </form>
+      ) : (
+        <p>
+          You need to be logged in to register your expenses for a project. Please{' '}
+          <Link to="/login">login</Link> or <Link to="/signup">signup.</Link>
+        </p>
+      )}
+    </>
+  ); 
 
   return (
     <div>
