8b33d158256dd6fa570ac4d47f6bb59696630709
nearcatch <692914+nearcatch@users.noreply.github.com>
2022-06-09 14:44:16 -0500

    removed unused imports

--- client/src/components/ProjectList/index.js
+++ client/src/components/ProjectList/index.js
@@ -1,17 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import TextField from "@mui/material/TextField";
-import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemText from "@mui/material/ListItemText";
 
 const ProjectList = ({
   projects,
