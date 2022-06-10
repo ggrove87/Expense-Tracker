4d94ed131523de5eec559dd2d0ab67085e247b85
Lindy Briggs <briggs.lindy@yahoo.com>
2022-06-10 11:37:45 -0500

    kevin is the star

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,10 +4,13 @@ import Typography from "@mui/material/Typography";
 const Home = () => {
   return (
     <main>
-      <Typography align="center" variant="h6" mt={5}>
+      <Typography align="center" variant="h6" my={5}>
         Welcome to the Expense Tracker, where monitoring home-project budgets is
         made easy.
       </Typography>
+      <div align="center">
+        <iframe src="https://giphy.com/embed/SEWEmCymjv8XDbsb8I" style={{ maxWidth: "400vw", height: 'auto' }} frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+      </div>
     </main>
   );
 };
