57d64a020f18566a335fe1f91de766fc4024a7dd
nearcatch <692914+nearcatch@users.noreply.github.com>
2022-06-10 12:39:37 -0500

    form as a row

--- client/src/components/ExpenseForm/index.js
+++ client/src/components/ExpenseForm/index.js
@@ -1,25 +1,25 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-import TextField from '@mui/material/TextField';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Alert from '@mui/material/Alert';
+import TextField from "@mui/material/TextField";
+import Input from "@mui/material/Input";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Alert from "@mui/material/Alert";
 
-import { useMutation } from '@apollo/client';
-import { ADD_EXPENSE } from '../../utils/mutations';
+import { useMutation } from "@apollo/client";
+import { ADD_EXPENSE } from "../../utils/mutations";
 
-import Auth from '../../utils/auth';
+import Auth from "../../utils/auth";
 
 const ExpenseForm = ({ projectId }) => {
-  const [expenseText, setExpenseText] = useState('');
-  const [expenseCount, setExpenseCount] = useState('');
-  const [expensePrice, setExpensePrice] = useState('');
-
+  const [expenseText, setExpenseText] = useState("");
+  const [expenseCount, setExpenseCount] = useState("");
+  const [expensePrice, setExpensePrice] = useState("");
 
   const [addExpense, { error }] = useMutation(ADD_EXPENSE);
 
@@ -32,13 +32,13 @@ const ExpenseForm = ({ projectId }) => {
           projectId,
           expenseText,
           expenseCount,
-          expensePrice
+          expensePrice,
         },
       });
 
-      setExpenseText('');
-      setExpenseCount(0)
-      setExpensePrice(0.00)
+      setExpenseText("");
+      setExpenseCount(0);
+      setExpensePrice(0.0);
     } catch (err) {
       console.error(err);
     }
@@ -47,60 +47,63 @@ const ExpenseForm = ({ projectId }) => {
   const handleChange = (event) => {
     const { name, value } = event.target;
 
-    if (name === 'expenseText') {
-      setExpenseText(value);  
+    if (name === "expenseText") {
+      setExpenseText(value);
     }
-    if (name === 'expenseCount') {
-      let intValue = Number(value)
-      setExpenseCount(intValue);  
+    if (name === "expenseCount") {
+      let intValue = Number(value);
+      setExpenseCount(intValue);
     }
-    if (name === 'expensePrice') {
-      let intValue = Number(value)
-      setExpensePrice(intValue); 
+    if (name === "expensePrice") {
+      let intValue = Number(value);
+      setExpensePrice(intValue);
     }
   };
 
   return (
     <>
-
-{Auth.loggedIn() ? (
-      <form onSubmit={handleFormSubmit}>
-        <Card align="center" sx={{ border: 1, maxWidth: 275, mx: 'auto', mt: '2rem' }}>
-          <CardContent>
-            <Typography color="text.secondary" variant="h5" gutterBottom>What expense are you adding for this project?</Typography>
-
-            <Box sx={{ '& > :not(style)': { m: 1, width: '25ch' }, }} noValidate autoComplete="off">
-
-            <TextField id="outlined-basic" label="item..." variant="outlined"
-                name="expenseText"
-                value={expenseText}
-                onChange={handleChange} />
-
-              <TextField id="outlined-basic" label="unit count..." variant="outlined"
-                name="expenseCount"
-                type="number"
-                value={expenseCount}
-                onChange={handleChange} />
-
-              <TextField id="outlined-basic" label="unit price..." variant="outlined"
-                name="expensePrice"
-                type="number"
-                value={expensePrice}
-                onChange={handleChange} />
-            </Box>
-
-          </CardContent>
-
-          <CardActions style={{ justifyContent: 'center' }}>
-            <Button size="small" style={{ cursor: 'pointer' }} sx={{ mb: '1rem' }} type="submit" variant="outlined">Add Expense</Button>
-          </CardActions>
-
-        </Card>
-      </form>
+      {Auth.loggedIn() ? (
+        <form
+          onSubmit={handleFormSubmit}
+          // style={{ marginLeft: "2rem", marginRight: "2rem" }}
+        >
+          <Input
+            placeholder="item..."
+            name="expenseText"
+            value={expenseText}
+            onChange={handleChange}
+            sx={{ width: "25%", paddingLeft: "1rem" }}
+          />
+          <Input
+            placeholder="unit count..."
+            name="expenseCount"
+            type="number"
+            value={expenseCount}
+            onChange={handleChange}
+            sx={{ width: "25%", paddingLeft: "2rem", textAlign: "right" }}
+          />
+          <Input
+            placeholder="unit price..."
+            name="expensePrice"
+            type="number"
+            value={expensePrice}
+            onChange={handleChange}
+            sx={{ width: "25%", paddingLeft: "2rem" }}
+          />
+          <Button
+            size="small"
+            style={{ width: "25%", cursor: "pointer" }}
+            type="submit"
+            variant="outlined"
+          >
+            ➕
+          </Button>
+        </form>
       ) : (
         <p>
-          You need to be logged in to register your expenses for a project. Please{' '}
-          <Link to="/login">login</Link> or <Link to="/signup">signup.</Link>
+          You need to be logged in to register your expenses for a project.
+          Please <Link to="/login">login</Link> or{" "}
+          <Link to="/signup">signup.</Link>
         </p>
       )}
     </>
