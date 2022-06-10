a4d02b27ab0d4b4ed444268549f39c37c5e394a4
nearcatch <692914+nearcatch@users.noreply.github.com>
2022-06-10 10:35:32 -0500

    table for expenses

--- client/src/components/ExpenseList/index.js
+++ client/src/components/ExpenseList/index.js
@@ -1,42 +1,58 @@
 import React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
 
-const ExpenseList = ({ expenses = [] }) => {
+const ExpenseList = ({ expenses = [], projectTitle }) => {
   // let [totalCost, setTotalCost] = useState(0);
-  let totalCost = 0 
+  let totalCost = 0;
   if (!expenses.length) {
     return <h3>No Expenses Yet</h3>;
   }
 
   for (let i = 0; i < expenses.length; i++) {
-    totalCost += expenses[i].expenseCount * expenses[i].expensePrice
+    totalCost += expenses[i].expenseCount * expenses[i].expensePrice;
   }
 
   return (
     <>
-      <h3
-        className="p-5 display-inline-block"
-        style={{ borderBottom: '1px dotted #1a1a1a' }}
-      >
-        Expenses
-      </h3>
-      <div className="flex-row my-4">
-        {expenses &&
-          expenses.map((expense) => (
-            <div key={expense._id} className="col-12 mb-3 pb-3">
-              <div className="p-3 bg-dark text-light">
-                <h5 className="card-header"> Expense Item: 
-                {expense.expenseText}
-                </h5>
-                <p className="card-body">Number of Units: {expense.expenseCount}</p>
-                <p className="card-body">Unit Price: {expense.expensePrice}</p>
-                <p className="card-body">Total Line Cost: 
-                    {expense.expenseCount * expense.expensePrice}
-                  </p>
-              </div>
-            </div>
-          ))}
-          <p>Project Total Cost:{totalCost}</p>
-      </div>
+      <Typography color="text.primary" variant="h5" m={2}>
+        {projectTitle} expenses - Current Total: {totalCost}
+      </Typography>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Item</TableCell>
+              <TableCell align="right">Unit Count</TableCell>
+              <TableCell align="right">Unit Price</TableCell>
+              <TableCell align="right">Total Line Cost</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {expenses.map((expense, index) => (
+              <TableRow
+                key={index}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="expense">
+                  {expense.expenseText}
+                </TableCell>
+                <TableCell align="right">{expense.expenseCount}</TableCell>
+                <TableCell align="right">{expense.expensePrice}</TableCell>
+                <TableCell align="right">
+                  {expense.expenseCount * expense.expensePrice}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </>
   );
 };
--- client/src/pages/SingleProject.js
+++ client/src/pages/SingleProject.js
@@ -1,14 +1,13 @@
-import React from 'react';
+import React from "react";
 
 // Import the `useParams()` hook
-import { useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
+import { useParams } from "react-router-dom";
+import { useQuery } from "@apollo/client";
 
-import ExpenseList from '../components/ExpenseList';
-import ExpenseForm from '../components/ExpenseForm';
+import ExpenseList from "../components/ExpenseList";
+import ExpenseForm from "../components/ExpenseForm";
 
-import { QUERY_SINGLE_PROJECT } from '../utils/queries';
-
+import { QUERY_SINGLE_PROJECT } from "../utils/queries";
 
 const SingleProject = () => {
   // Use `useParams()` to retrieve value of the route parameter `:profileId`
@@ -25,31 +24,14 @@ const SingleProject = () => {
     return <div>Loading...</div>;
   }
   return (
-    <div className="my-3">
-      <h3 className="card-header bg-dark text-light p-2 m-0">
-        {project.projectAuthor} <br />
-        <span style={{ fontSize: '1rem' }}>
-          had this project on {project.createdAt}
-        </span>
-      </h3>
-      <div className="bg-light py-4">
-        <blockquote
-          className="p-4"
-          style={{
-            fontSize: '1.5rem',
-            fontStyle: 'italic',
-            border: '2px dotted #1a1a1a',
-            lineHeight: '1.5',
-          }}
-        >
-          {project.projectText}
-        </blockquote>
+    <div>
+      <div>
+        <ExpenseList
+          expenses={project.expenses}
+          projectTitle={project.projectTitle}
+        />
       </div>
-
-      <div className="my-5">
-        <ExpenseList expenses={project.expenses} />
-      </div>
-      <div className="m-3 p-4" style={{ border: '1px dotted #1a1a1a' }}>
+      <div>
         <ExpenseForm projectId={project._id} />
       </div>
     </div>
